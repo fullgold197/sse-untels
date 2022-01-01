be97b979f7a61463df845bf8a4c8e8c8256920f0
Oscar Vilca Rivera <oevr1997@gmail.com>
2022-01-01 16:59:46 -0500

    Migraciones

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
-/*  DB::statement('SET SESSION sql_require_primary_key=0'); */
+
 class CreatePasswordResetsTable extends Migration
 {
     /**
