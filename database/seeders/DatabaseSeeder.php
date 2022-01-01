737f937be14abaac4c538424327a9b3dfd892e2b
Oscar Vilca Rivera <oevr1997@gmail.com>
2022-01-01 17:11:39 -0500

    truncable

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,14 +16,7 @@ class DatabaseSeeder extends Seeder
     {
 
         // \App\Models\User::factory(10)->create();
-        $this->truncatetables([
-            'academico',
-            'maestria',
-            'doctorado',
-            'egresado',
-            'profesion',
-            'users'
-        ]);
+        
         $this->call(AcademicoSeeder::class);
 
 
@@ -34,19 +27,7 @@ class DatabaseSeeder extends Seeder
         $this->call(UsuarioSeeder::class); //invocar al seeder de usuarios(tabla que depende de egresados mediante la llave foranea egresado_matricula)
     }
 
-    function truncatetables(array $tables){
 
-       /*  DB::statement('SET FOREIGN_KEY_CHECKS=0;'); */ //deshabilitar la revision de claves foráneas
-       /*  DB::table('egresado')->truncate(); */ //para vaciar las tablas antes de  volver a ejecutar los registros nuevamente
-
-       /*  foreach ($tables as $table){
-            DB::table($table)->truncate(); //para vaciar las tablas antes de  volver a ejecutar los registros nuevamente
-
-        } */
-        /* DB::statement('SET FOREIGN_KEY_CHECKS=1;'); */ //habilitar la revision de claves foráneas
-
-
-    }
 
 }
 
