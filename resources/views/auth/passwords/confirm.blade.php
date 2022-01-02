9f15440924857c0dc84a791d541b80331564b5d0
Oscar Vilca Rivera <oevr1997@gmail.com>
2022-01-02 14:10:37 -0500

    email y correcion de responsive

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,9 +1,9 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8" style="margin:70px;align=center">
             <div class="card">
                 <div class="card-header">{{ __('Confirm Password') }}</div>
 
