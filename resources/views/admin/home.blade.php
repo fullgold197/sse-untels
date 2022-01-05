eed60e8cc7ad5371823a0ea3e604125a2bf269a1
Oscar Vilca Rivera <oevr1997@gmail.com>
2022-01-04 20:59:22 -0500

    Mensaje de bienvenida

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,12 +3,12 @@
 @section('title', 'Admin')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>Inicio</h1>
 @stop
 
 @section('content')
 
-    <p>Dashboard</p>
+    <p>Bienvenido(a) {{Auth::user()->name}} al sistema web de seguimiento de egresados. Aqui podrás monitorear a los egresados con respecto a las maestrías y doctorados que hagan, así como los empleos que hagan en el transcurso de su profesión.</p>
 
 @stop
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.egresado')
 @section('content')
-<h5>hola</h5>
+<h5>Inicio</h5>
+<p>Bienvenido(a) {{Auth::user()->name}} al sistema web de seguimiento de egresados. Aqui podrás registrar tus estudios postgrados con respecto a las maestrías y doctorados que hagan, así como los empleos que hagas en el transcurso de tu profesión.</p>
 @endsection
